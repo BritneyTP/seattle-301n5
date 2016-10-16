cde44c7dc1e3830e58e0ce7f0d2e6d2291ad965d
Britney <britney.presley26@gmail.com>
2016-10-16 15:23:13 -0700

    lab9 states and cities populating

--- class-09-joins-and-relations/lab/zip-codes/scripts/search.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/search.js
@@ -1,7 +1,39 @@
 (function(module) {
 
   // TODO: Write the code to populate your filters, and enable the search queries here in search.js
-  // TODO: You will also interact with the map.js file here
+  function populateFilters(){}
 
+  populateFilters.statesPop = function(){
+    webDB.execute([{
+      'sql': 'SELECT DISTINCT state FROM zips ORDER BY state ',
+    }
+  ],
+  function(result){
+    result.forEach(function(ind){
+      $('#state-select').append('<option>' + ind.state + '</option>')
 
+    })
+
+  }
+);
+};
+
+populateFilters.cityPop = function(){
+  $('#state-select').on('change', function(){
+    webDB.execute([{
+      'sql': 'SELECT DISTINCT city FROM zips WHERE state = ? ORDER BY city',
+      'data' : [$('#state-select').val()]
+    }
+  ],
+  function(result){
+    result.forEach(function(ind){
+      $('#city-select').append('<option>' + ind.city + '</option>')
+    })
+  });
+})
+
+}
+// TODO: You will also interact with the map.js file here
+
+module.populateFilters = populateFilters;
 })(window)
