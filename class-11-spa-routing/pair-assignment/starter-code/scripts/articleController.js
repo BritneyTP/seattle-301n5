a874250b299602a6fd30be6d43cdd3c479cd6a07
Britney <britney.presley26@gmail.com>
2016-10-23 14:42:21 -0700

    pair-programming with Will

--- class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
@@ -1,13 +1,17 @@
 (function(module) {
   var articlesController = {};
 
-  // TODO: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
+  // TODID: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
+  Article.createTable();
 
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // TODID: Setup a function that kicks off the fetching and rendering of articles, using the same
+  Article.fetchAll(articleView.initIndexPage);
   // code that used to be in index.html.
-  // TODO: Also be sure to hide all the main section elements, and reveal the #articles section:
+  // TODID: Also be sure to hide all the main section elements, and reveal the #articles section:
   articlesController.index = function() {
+    $('.tab-content').hide();
 
+    $('#articles').show();
   };
 
   module.articlesController = articlesController;
--- class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
@@ -1,4 +1,9 @@
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
-// linked to a a single controller function to handle it:
-
-// TODO: What function do you call to activate page.js? Fire it off now, to execute!
+'use strict';
+// TODID: Configure routes for this app with page.js, by registering each URL your app can handle,
+// linked to a a single controller function to handle it: page('/', index)
+page('/',articlesController.index() )
+// page('/admin', )
+page('/about',aboutController.index() )
+// page('/*', homeController.error)
+// TODID: What function do you call to activate page.js? Fire it off now, to execute!
+page();
