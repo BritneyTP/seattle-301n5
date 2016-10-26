8de3956a9dcbc8bef85b4d24b2af9591c0f0451c
Britney <britney.presley26@gmail.com>
2016-10-26 14:30:51 -0700

    added scripts tags to the html in order for the page to read the code correctly

--- class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
@@ -2,8 +2,11 @@
 // TODID: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it: page('/', index)
 page('/',articlesController.index() )
-// page('/admin', )
+// page('/about', )
 page('/about',aboutController.index() )
+// page('/admin', )
+// page('/admin')
+
 // page('/*', homeController.error)
 // TODID: What function do you call to activate page.js? Fire it off now, to execute!
 page();
