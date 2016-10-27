2ab92afb16ea638cd7910fa52c19cd80b795479c
Britney <britney.presley26@gmail.com>
2016-10-26 22:59:58 -0500

    pair programming with Will

---
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
@@ -0,0 +1,38 @@
+'use strict';
+
+(function(module) {
+  var repos = {};
+
+  repos.all = [];
+
+  repos.requestRepos = function(callback) {
+    // TODID: How would you like to fetch your repos? Don't forget to call the callback.
+    $.ajax({
+      url: 'https://api.github.com/user/repos?sort=updated&per_page=10',
+      type: 'GET',
+      headers: {
+        Authorization: 'token ' + githubToken
+      }
+    }).done(function(data) {
+      console.log(data);
+      data.filter(function(ele) {
+        return ele.owner.login === 'britney-t-presley';
+      }).forEach(function(repo) {
+        var output = '<h2>' + repo.name + '</h2>' +
+          '<p>' + repo.description + '</p>';
+        $('#repos').append(output);
+      })
+    });
+
+  };
+
+  // DONE: Model method that filters the full collection for repos with a particular attribute.
+  // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
